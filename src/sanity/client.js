ddf306dd57c653466c8516931aed5334f69303fb
Samuel Donkor <calvinsamueldonkor@gmail.com>
2026-02-05 15:03:23 +0000

    Update environment variables to replace ButterCMS with Sanity.io configuration. Modify client.js to use new Sanity project ID.

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID || 's7v9yaup',
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID || 'ijklcof1',
   dataset: process.env.REACT_APP_SANITY_DATASET || 'production',
   apiVersion: '2024-01-01',
   useCdn: true, // Set to false if you want fresh data
